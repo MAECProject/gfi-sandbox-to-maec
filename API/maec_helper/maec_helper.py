6f4b7e8b4af8498424a07ec24cb76704662dd818
Ivan Kirillov <ikirillov@mitre.org>
2012-11-26 17:39:49 -0500

    Fixed import naming, added stubs for maec_package and maec_subject

--- API/maec_helper/maec_helper.py
+++ API/maec_helper/maec_helper.py
@@ -6,8 +6,8 @@
 #Compatible with MAEC v3.0
 #Last updated 11/26/2012
 
-import maec_bundle_3_0 as maec_bundle
-import maec_package_1_0 as maec_package
+import maec_bundle_3_0 as maecbundle
+import maec_package_1_0 as maecpackage
 import cybox.file_object_1_3 as file_object
 import cybox.socket_object_1_4 as socket_object
 import cybox.process_object_1_3 as process_object
@@ -137,6 +137,18 @@ class generator:
     #Methods for getting current id bases
     def get_current_obj_id(self):
         return self.obj_id_base
+
+class maec_package:
+    def __init__(self, generator, schema_version):
+        self.generator = generator
+        #Create the MAEC Package object
+        self.package = maecpackage.PackageType(id=self.generator.generate_pkg_id())
+
+class maec_subject:
+    def __init__(self, generator, schema_version):
+        self.generator = generator
+        #Create the MAEC Subject object
+        self.subject = maecpackage.MalwareSubjectType(id=self.generator.generate_sub_id())
     
 class maec_bundle:
     def __init__(self, generator, schema_version):
