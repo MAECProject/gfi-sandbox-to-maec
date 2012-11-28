855a4840a5cbe9880649127f2311dc4694331bdc
Ivan Kirillov <ikirillov@mitre.org>
2012-11-28 10:20:40 -0500

    For accuracy in maec_object, renamed create_file_system_object to create_win_file_object and added a new create_file_object method

--- API/maec_helper/maec_helper.py
+++ API/maec_helper/maec_helper.py
@@ -930,14 +930,58 @@ class maec_object:
             cybox_object.set_Defined_Object(reg_object)
         
         return cybox_object
-  
-    def create_file_system_object(self, file_system_attributes):
+
+    def create_file_object(self, file_attributes):
+        cybox_object = maecbundle.cybox_core_1_0.AssociatedObjectType(id=self.generator.generate_obj_id())
+        fileobj = win_file_object.WindowsFileObjectType()
+        fileobj.set_anyAttributes_({'xsi:type' : 'FileObj:FileObjectType'})
+        cybox_object.set_type('File')
+        fs_hashes = maecbundle.cybox_common_types_1_0.HashListType()
+        
+        for key, value in file_attributes.items():
+            if key == 'md5':
+                if self.__value_test(value):
+                    hash_value = maecbundle.cybox_common_types_1_0.HexBinaryObjectAttributeType(datatype='hexBinary', valueOf_=maecbundle.quote_xml(value))
+                    hash_type = maecbundle.cybox_common_types_1_0.HashNameType(datatype='String', valueOf_='MD5')
+                    hash = maecbundle.cybox_common_types_1_0.HashType(Simple_Hash_Value=hash_value, Type=hash_type)
+                    fs_hashes.add_Hash(hash)
+            elif key == 'sha1':
+                if self.__value_test(value):
+                    hash_value = maecbundle.cybox_common_types_1_0.HexBinaryObjectAttributeType(datatype='hexBinary', valueOf_=maecbundle.quote_xml(value))
+                    hash_type = maecbundle.cybox_common_types_1_0.HashNameType(datatype='String', valueOf_='SHA1')
+                    hash = maecbundle.cybox_common_types_1_0.HashType(Simple_Hash_Value=hash_value, Type=hash_type)
+                    fs_hashes.add_Hash(hash)
+            elif key == 'packer':
+                if self.__value_test(value):
+                    packer_list = file_object.PackerListType()
+                    packer = file_object.PackerAttributesType(Name=maecbundle.cybox_common_types_1_0.StringObjectAttributeType(datatype='String', valueOf_=maecbundle.quote_xml(value)))
+                    packer_list.add_Packer(packer)
+                    fileobj.set_Packer_List(packer_list)
+            elif key == 'av_aliases':
+                cybox_object.set_Domain_specific_Object_Attributes(value)
+            elif key == 'filename':
+                if self.__value_test(value):
+                    fileobj.set_File_Name(maecbundle.cybox_common_types_1_0.StringObjectAttributeType(datatype='String', valueOf_=maecbundle.quote_xml(value)))
+            elif key == 'filepath':
+                if self.__value_test(value):
+                    filepath = maecbundle.cybox_common_types_1_0.StringObjectAttributeType(datatype='String', valueOf_=maecbundle.quote_xml(value))
+                    fileobj.set_File_Path(filepath)
+
+        if fs_hashes.hasContent_():
+            fileobj.set_Hashes(fs_hashes)
+        
+        if fileobj.hasContent_():
+            cybox_object.set_Defined_Object(fileobj)
+
+        return cybox_object
+
+    def create_win_file_object(self, win_file_attributes):
         cybox_object = maecbundle.cybox_core_1_0.AssociatedObjectType(id=self.generator.generate_obj_id())
         fileobj = win_file_object.WindowsFileObjectType()
         fileobj.set_anyAttributes_({'xsi:type' : 'WinFileObj:WindowsFileObjectType'})
         cybox_object.set_type('File')
         fs_hashes = maecbundle.cybox_common_types_1_0.HashListType()
-        for key, value in file_system_attributes.items():
+        for key, value in win_file_attributes.items():
             if key == 'md5':
                 if self.__value_test(value):
                     hash_value = maecbundle.cybox_common_types_1_0.HexBinaryObjectAttributeType(datatype='hexBinary', valueOf_=maecbundle.quote_xml(value))
